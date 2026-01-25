561ecb1d9ee9f69f0d5f10842d4c7e2a61a93b7e
Abu Shamsutdinov <stuneak@gmail.com>
2026-01-26 02:55:28 +0300

    fix db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
 
 export const authOptions: NextAuthOptions = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   adapter: PrismaAdapter(db) as any,
   providers: [
     GoogleProvider({
